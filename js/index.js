7405c13fe4588150a06308daee516c027bb50288
FLWorksTest <taherkhamaysa@gmail.com>
2020-12-21 19:39:42 +0300

    commit 3

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 
   $(".mobile-nav-toggle").click(function () {
     $("#mobileSideNav").toggleClass("show", 1000);
+    $("body").toggleClass("show-mobile-nav", 1000);
   });
 
   $(".drop-down-link").click(function () {
