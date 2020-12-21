07c8598d6e3831c783d7d3c6cc9290cb427fdcc1
FLWorksTest <taherkhamaysa@gmail.com>
2020-12-21 17:15:48 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+  $(".mobile-nav-toggle").click(function () {
+    $("#mobileSideNav").toggleClass("show", 1000);
+  });
+
+  $(".drop-down-link").click(function () {
+    $(this).parent().toggleClass("show-drop-down", 1000);
+  });
+
+});
+
